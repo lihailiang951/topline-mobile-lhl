14ee77ee0dd68d887b184ba8dd9718668ffe1c1d
尹大炮 <3235144364@qq.com>
2019-07-08 20:40:30 +0800

    项目搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
